8b3355dd8989e38860d7efd9a051180a6ccee227
mahmoud hamada <mahouudhamada@gmail.com>
2026-07-20 17:08:36 +0300

    save latest changes

--- project chat.cpp
+++ project chat.cpp
@@ -2,6 +2,7 @@
 #include <iostream>
 #include<string>
 #include <vector>
+#include<fstream>
 
 using namespace std;
 struct Student
@@ -11,10 +12,36 @@ struct Student
     int id;
     float gpa;
 
-};
-vector<Student> students;
-
+};//sttuctفيها المحتويال لكل طالب مع بغعضها 
+vector<Student> students;// لتخزين الطلاب في مصفوفة 
+void saveToFile()
+{
+    ofstream outfile("students.txt");
+    for (int i = 0;i < students.size();i++)
+    {
+        outfile << students[i].name << " "
+            << students[i].age << " "
+            << students[i].id << " "
+            << students[i].gpa << "\n";//اضافه العناصر للمصفوفه علي سطر واحد
+    }
+    outfile.close();// قفل الملف بعد الانتهاء
+}
 //int studentcount=0;
+void loadFromFile()
+{
+    ifstream infile("students.txt");
+    if (!infile)
+    {
+        return;
+    }
+    Student temp;
+    while (infile >> temp.name >> temp.age >> temp.id >> temp.gpa)
+    {
+        students.push_back(temp);//اضافة العناصر للمصفوفه
+    }
+    infile.close();// قفل الملف بعد الانتهاء
+
+}
 
 
 void addStudent() 
@@ -34,9 +61,11 @@ void addStudent()
     cout<<"gpa: ";
     cin>> newStudent.gpa;
    /* studentcount++;*/
-   students.push_back(newStudent);
+	students.push_back(newStudent);//اضافة الطالب إلى المصفوفة
+   saveToFile();// حفظ البيانات في ملف  بعد إضافة الطالب
     cout<<"Student Added Successfully\n";
 }
+
 void showStudent()
 {
     
@@ -58,7 +87,8 @@ void showStudent()
        
 
 }
-void SearchStudent()
+
+void searchStudent()
 {   
     int searchid;
     cout<<"ENTER ID: ";
@@ -80,7 +110,8 @@ void SearchStudent()
     }
     if (!found)cout << "Not found" << "\n";
 }
-void  DeleteStudent()
+
+void  deleteStudent()
 {
     int searchId;
     cout<<"ENTER ID: ";
@@ -90,13 +121,14 @@ void  DeleteStudent()
     {
         if(searchId== students[i].id)
         {
+            found = true;
             for(int j=i;j< students.size() -1;j++)
             {
                 students[j]=students[j+1];
                 /*students[j].names= students[j+1].names;
                 students[j].ages= students[j+1].ages;
                 students[j].ids= students[j+1].ids;
-                students[j].gpas= students[j+1].gpas;*/
+                students[j].gpas= students[j+1].gpas;*/ 
             }
 			students.pop_back();// حذف آخر عنصر من المصفوفة بعد نقل العناصر
 			saveToFile();// حفظ البيانات في ملف  بعد حذف الطالب
@@ -116,13 +148,48 @@ void  DeleteStudent()
 
 }
 
+void updateStudent()
+{
+	cout << "ENTER ID: ";
+	int searchID;
+	cin >> searchID;
+    bool found = false;
+	for (int i = 0;i < students.size();i++)
+	{
+	    if(searchID == students[i].id)
+        {
+			cout << "ENTER NEW NAME: ";
+			cin >> students[i].name;
+			cout << "ENTER NEW AGE: ";
+			cin >> students[i].age;
+			cout << "ENTER NEW GPA: ";
+			cin >> students[i].gpa;
+			found = true;
+			saveToFile();// حفظ البيانات في ملف  بعد تحديث الطالب
+			break;
+        
+        }
+	}
+	 if (!found)
+     {
+		 cout << "Student Not found\n";
+     }
+     else
+     {
+         cout << "Student Updated Successfully\n";
+     }
+
+
+}
+
+
 int main()
 { 
-    bool val = false;
+	loadFromFile();// تحميل البيانات من الملف عند بدء البرنامج
     while(true){
 
         cout << "===== Student Management System =====" << "\n\n" <<
-            "1. Add Student\n2. Show Students\n3. Search student\n4. Delete student\n5. Exit" 
+            "1. Add Student\n2. Show Students\n3. Search student\n4. Delete student\n5. Update student\n6. Exit" 
             <<"\n\n" << "choose: ";
 
         int  choose;
@@ -132,13 +199,14 @@ int main()
         {
         case 1: addStudent();break;
         case 2: showStudent();break;
-        case 3: SearchStudent();break;
-        case 4: DeleteStudent();break;
-        case 5: cout << "Good Bye";return 0;
-        default:val=true; cout << "Invalid Choices\n";
+        case 3: searchStudent();break;
+        case 4: deleteStudent();break;
+		case 5: updateStudent();break;
+		case 6: cout << "Good Bye";return 0;
+        default: cout << "Invalid Choices\n";
         }
 
-        };
+    }
    
     
 }
